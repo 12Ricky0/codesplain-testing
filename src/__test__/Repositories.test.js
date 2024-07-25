f40175c528f21103ac6cf58cb6bd394dcd823611
Richard Addo <ramankwah03@gmail.com>
2024-07-25 16:58:14 +0200

    test added

--- src/__test__/Repositories.test.js
+++ src/__test__/Repositories.test.js
@@ -1,7 +1,31 @@
 import { render, screen } from "@testing-library/react";
 import RepositoriesSummary from "../components/repositories/RepositoriesSummary";
+import RepositoriesListItem from "../components/repositories/RepositoriesListItem";
+import { MemoryRouter } from "react-router";
 
-describe("RepositoriesSummary", () => {
+// You can mock a component by passing: This can be done the sole the 'act' problem
+// jest.mock('../components/tree/FileIcon', () => {
+//     return () => {
+//         "FileIcon"
+//     }
+// })
+
+function renderListComponent() {
+    const mockRepository = {
+        full_name: "test/repo",
+        name: "repo",
+        description: "A test repository",
+        language: "JavaScript",
+        html_url: "https://github.com/test/repo",
+        owner: 'facebook'
+    }
+    render(
+        <MemoryRouter>
+            <RepositoriesListItem repository={mockRepository} />
+        </MemoryRouter>
+    );
+}
+describe("Repositories Testing", () => {
     it("displays information about the repository", () => {
         const repo = {
             language: "JavaScript",
@@ -13,5 +37,13 @@ describe("RepositoriesSummary", () => {
         for (let key in repo) {
             expect(screen.getByText(new RegExp(repo[key]))).toBeInTheDocument();
         }
+    })
+
+    it("shows a link to the repository", async () => {
+        renderListComponent();
+        //use find.. to solve asynchronous functionality
+        await screen.findByRole('img', {
+            nme: /JavaScript/i
+        })
     })
 })
