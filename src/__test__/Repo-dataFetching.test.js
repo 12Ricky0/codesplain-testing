aca2a6cdf1c9abee8d35b40730f7652300838528
Richard Addo <ramankwah03@gmail.com>
2024-07-26 08:54:47 +0200

    server added

--- src/__test__/Repo-dataFetching.test.js
+++ src/__test__/Repo-dataFetching.test.js
@@ -1,28 +1,24 @@
 import { screen, render } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 import HomeRoute from "../routes/HomeRoute";
-import { rest } from "msw";
-import { setupServer } from "msw/node";
+import { createServer } from "./server";
 
-const hanler = [
-    rest.get("/api/repositories", (req, res, ctx) => {
-        const language = (req.url.searchParams.get('q')).split(':')[2]
-        return res(
-            ctx.json({
+createServer([
+    {
+        path: "/api/repositories",
+        res: (req, res, ctx) => {
+            const language = (req.url.searchParams.get('q')).split(':')[2]
+
+            return {
                 items: [
                     { id: 1, full_name: language + '_one' },
                     { id: 2, full_name: language + '_two' },
                 ]
-            })
-        );
-    }),
-];
+            }
+        }
+    },
+])
 
-const server = setupServer(...hanler);
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
 
 describe("testing data fetching", () => {
     test("renders search results", async () => {
@@ -39,6 +35,8 @@ describe("testing data fetching", () => {
             })
             expect(link).toHaveLength(2); // 2 repositories for each language
         }
+
+
 
     });
 })
